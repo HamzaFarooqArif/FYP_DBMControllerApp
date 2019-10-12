5ff8fecf70224696b53c9e19c8c879be942c999e
Hamza <m.hamza.arif786@gmail.com>
2019-10-12 09:36:23 +0500

    NewAppCommit

---
+++ DBMControllerApp_TK/CameraProperties.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace DBMControllerApp_TK
+{
+    partial class CameraProperties
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Capture1 = new System.Windows.Forms.Button();
+            this.btn_Preview1 = new System.Windows.Forms.Button();
+            this.cb_1 = new System.Windows.Forms.ComboBox();
+            this.trk_HueU1 = new System.Windows.Forms.TrackBar();
+            this.rtb_U1 = new System.Windows.Forms.RichTextBox();
+            this.trk_SatU1 = new System.Windows.Forms.TrackBar();
+            this.trk_ValU1 = new System.Windows.Forms.TrackBar();
+            this.tb_HueU1 = new System.Windows.Forms.TextBox();
+            this.tb_SatU1 = new System.Windows.Forms.TextBox();
+            this.tb_ValU1 = new System.Windows.Forms.TextBox();
+            this.tb_ValL1 = new System.Windows.Forms.TextBox();
+            this.tb_SatL1 = new System.Windows.Forms.TextBox();
+            this.tb_HueL1 = new System.Windows.Forms.TextBox();
+            this.trk_ValL1 = new System.Windows.Forms.TrackBar();
+            this.trk_SatL1 = new System.Windows.Forms.TrackBar();
+            this.rtb_L1 = new System.Windows.Forms.RichTextBox();
+            this.trk_HueL1 = new System.Windows.Forms.TrackBar();
+            this.lbl_selectCamera1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trk_HueU1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trk_SatU1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trk_ValU1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trk_ValL1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trk_SatL1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trk_HueL1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Capture1
+            // 
+            this.btn_Capture1.Location = new System.Drawing.Point(12, 39);
+            this.btn_Capture1.Name = "btn_Capture1";
+            this.btn_Capture1.Size = new System.Drawing.Size(96, 23);
+            this.btn_Capture1.TabIndex = 0;
+            this.btn_Capture1.Text = "Capture";
+            this.btn_Capture1.UseVisualStyleBackColor = true;
+            this.btn_Capture1.Click += new System.EventHandler(this.btn_Capture1_Click);
+            // 
+            // btn_Preview1
+            // 
+            this.btn_Preview1.Location = new System.Drawing.Point(114, 39);
+            this.btn_Preview1.Name = "btn_Preview1";
+            this.btn_Preview1.Size = new System.Drawing.Size(101, 23);
+            this.btn_Preview1.TabIndex = 1;
+            this.btn_Preview1.Text = "Toggle Preview";
+            this.btn_Preview1.UseVisualStyleBackColor = true;
+            this.btn_Preview1.Click += new System.EventHandler(this.btn_Preview1_Click);
+            // 
+            // cb_1
+            // 
+            this.cb_1.FormattingEnabled = true;
+            this.cb_1.Location = new System.Drawing.Point(94, 12);
+            this.cb_1.Name = "cb_1";
+            this.cb_1.Size = new System.Drawing.Size(121, 21);
+            this.cb_1.TabIndex = 2;
+            // 
+            // trk_HueU1
+            // 
+            this.trk_HueU1.Location = new System.Drawing.Point(12, 68);
+            this.trk_HueU1.Name = "trk_HueU1";
+            this.trk_HueU1.Size = new System.Drawing.Size(154, 45);
+            this.trk_HueU1.TabIndex = 3;
+            // 
+            // rtb_U1
+            // 
+            this.rtb_U1.Location = new System.Drawing.Point(216, 80);
+            this.rtb_U1.Name = "rtb_U1";
+            this.rtb_U1.Size = new System.Drawing.Size(45, 45);
+            this.rtb_U1.TabIndex = 4;
+            this.rtb_U1.Text = "";
+            this.rtb_U1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // trk_SatU1
+            // 
+            this.trk_SatU1.Location = new System.Drawing.Point(12, 94);
+            this.trk_SatU1.Name = "trk_SatU1";
+            this.trk_SatU1.Size = new System.Drawing.Size(154, 45);
+            this.trk_SatU1.TabIndex = 5;
+            // 
+            // trk_ValU1
+            // 
+            this.trk_ValU1.Location = new System.Drawing.Point(12, 119);
+            this.trk_ValU1.Name = "trk_ValU1";
+            this.trk_ValU1.Size = new System.Drawing.Size(154, 45);
+            this.trk_ValU1.TabIndex = 6;
+            // 
+            // tb_HueU1
+            // 
+            this.tb_HueU1.Location = new System.Drawing.Point(172, 68);
+            this.tb_HueU1.Name = "tb_HueU1";
+            this.tb_HueU1.Size = new System.Drawing.Size(38, 20);
+            this.tb_HueU1.TabIndex = 7;
+            // 
+            // tb_SatU1
+            // 
+            this.tb_SatU1.Location = new System.Drawing.Point(172, 94);
+            this.tb_SatU1.Name = "tb_SatU1";
+            this.tb_SatU1.Size = new System.Drawing.Size(38, 20);
+            this.tb_SatU1.TabIndex = 8;
+            // 
+            // tb_ValU1
+            // 
+            this.tb_ValU1.Location = new System.Drawing.Point(172, 120);
+            this.tb_ValU1.Name = "tb_ValU1";
+            this.tb_ValU1.Size = new System.Drawing.Size(38, 20);
+            this.tb_ValU1.TabIndex = 9;
+            // 
+            // tb_ValL1
+            // 
+            this.tb_ValL1.Location = new System.Drawing.Point(172, 222);
+            this.tb_ValL1.Name = "tb_ValL1";
+            this.tb_ValL1.Size = new System.Drawing.Size(38, 20);
+            this.tb_ValL1.TabIndex = 16;
+            // 
+            // tb_SatL1
+            // 
+            this.tb_SatL1.Location = new System.Drawing.Point(172, 196);
+            this.tb_SatL1.Name = "tb_SatL1";
+            this.tb_SatL1.Size = new System.Drawing.Size(38, 20);
+            this.tb_SatL1.TabIndex = 15;
+            // 
+            // tb_HueL1
+            // 
+            this.tb_HueL1.Location = new System.Drawing.Point(172, 170);
+            this.tb_HueL1.Name = "tb_HueL1";
+            this.tb_HueL1.Size = new System.Drawing.Size(38, 20);
+            this.tb_HueL1.TabIndex = 14;
+            // 
+            // trk_ValL1
+            // 
+            this.trk_ValL1.Location = new System.Drawing.Point(12, 221);
+            this.trk_ValL1.Name = "trk_ValL1";
+            this.trk_ValL1.Size = new System.Drawing.Size(154, 45);
+            this.trk_ValL1.TabIndex = 13;
+            // 
+            // trk_SatL1
+            // 
+            this.trk_SatL1.Location = new System.Drawing.Point(12, 196);
+            this.trk_SatL1.Name = "trk_SatL1";
+            this.trk_SatL1.Size = new System.Drawing.Size(154, 45);
+            this.trk_SatL1.TabIndex = 12;
+            // 
+            // rtb_L1
+            // 
+            this.rtb_L1.Location = new System.Drawing.Point(216, 182);
+            this.rtb_L1.Name = "rtb_L1";
+            this.rtb_L1.Size = new System.Drawing.Size(45, 45);
+            this.rtb_L1.TabIndex = 11;
+            this.rtb_L1.Text = "";
+            // 
+            // trk_HueL1
+            // 
+            this.trk_HueL1.Location = new System.Drawing.Point(12, 170);
+            this.trk_HueL1.Name = "trk_HueL1";
+            this.trk_HueL1.Size = new System.Drawing.Size(154, 45);
+            this.trk_HueL1.TabIndex = 10;
+            // 
+            // lbl_selectCamera1
+            // 
+            this.lbl_selectCamera1.AutoSize = true;
+            this.lbl_selectCamera1.Location = new System.Drawing.Point(12, 15);
+            this.lbl_selectCamera1.Name = "lbl_selectCamera1";
+            this.lbl_selectCamera1.Size = new System.Drawing.Size(76, 13);
+            this.lbl_selectCamera1.TabIndex = 17;
+            this.lbl_selectCamera1.Text = "Select Camera";
+            // 
+            // CameraProperties
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 278);
+            this.Controls.Add(this.lbl_selectCamera1);
+            this.Controls.Add(this.tb_ValL1);
+            this.Controls.Add(this.tb_SatL1);
+            this.Controls.Add(this.tb_HueL1);
+            this.Controls.Add(this.trk_ValL1);
+            this.Controls.Add(this.trk_SatL1);
+            this.Controls.Add(this.rtb_L1);
+            this.Controls.Add(this.trk_HueL1);
+            this.Controls.Add(this.tb_ValU1);
+            this.Controls.Add(this.tb_SatU1);
+            this.Controls.Add(this.tb_HueU1);
+            this.Controls.Add(this.trk_ValU1);
+            this.Controls.Add(this.trk_SatU1);
+            this.Controls.Add(this.rtb_U1);
+            this.Controls.Add(this.trk_HueU1);
+            this.Controls.Add(this.cb_1);
+            this.Controls.Add(this.btn_Preview1);
+            this.Controls.Add(this.btn_Capture1);
+            this.Name = "CameraProperties";
+            this.Text = "Camera Properties";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trk_HueU1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trk_SatU1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trk_ValU1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trk_ValL1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trk_SatL1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trk_HueL1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Capture1;
+        private System.Windows.Forms.Button btn_Preview1;
+        private System.Windows.Forms.ComboBox cb_1;
+        private System.Windows.Forms.TrackBar trk_HueU1;
+        private System.Windows.Forms.RichTextBox rtb_U1;
+        private System.Windows.Forms.TrackBar trk_SatU1;
+        private System.Windows.Forms.TrackBar trk_ValU1;
+        private System.Windows.Forms.TextBox tb_HueU1;
+        private System.Windows.Forms.TextBox tb_SatU1;
+        private System.Windows.Forms.TextBox tb_ValU1;
+        private System.Windows.Forms.TextBox tb_ValL1;
+        private System.Windows.Forms.TextBox tb_SatL1;
+        private System.Windows.Forms.TextBox tb_HueL1;
+        private System.Windows.Forms.TrackBar trk_ValL1;
+        private System.Windows.Forms.TrackBar trk_SatL1;
+        private System.Windows.Forms.RichTextBox rtb_L1;
+        private System.Windows.Forms.TrackBar trk_HueL1;
+        private System.Windows.Forms.Label lbl_selectCamera1;
+    }
+}
+
---
+++ DBMControllerApp_TK/CameraProperties.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace DBMControllerApp_TK
+{
+    public partial class CameraProperties : Form
+    {
+        private bool camStart0 = false;
+        private bool camStart1 = false;
+
+        public CameraProperties()
+        {
+            InitializeComponent();
+
+            CvInvoke.UseOpenCL = false;
+            cb_1.DataSource = CameraUtility.getInstance(0).getCameraList();
+            Application.Idle += processFrame;
+        }
+
+        private void processFrame(object sender, EventArgs arg)
+        {
+            if(camStart0)
+            {
+                CameraUtility.getInstance(cb_1.SelectedIndex).processFrame();
+            }
+            if (camStart1)
+            {
+                CameraUtility.getInstance(1).processFrame();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Preview1_Click(object sender, EventArgs e)
+        {
+            if(camStart0)
+            {
+                CameraUtility.getInstance(0).isPreview = !CameraUtility.getInstance(0).isPreview;
+            }
+            else
+            {
+                MessageBox.Show("Please Start the device first");
+            }
+        }
+
+        private void btn_Capture1_Click(object sender, EventArgs e)
+        {
+            if (cb_1.Items.Count > 0)
+            {
+                camStart0 = true;
+            }
+            else MessageBox.Show("Please Select a valid capture device");
+            
+        }
+    }
+}
